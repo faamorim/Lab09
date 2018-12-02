c870507aea2a00f805d8d9a41698bb8486d8c4cb
faamorim <faamorim@gmail.com>
2018-12-02 08:43:21 -0800

    some comments

--- maze_game.cpp
+++ maze_game.cpp
@@ -4,7 +4,7 @@
 
 #include "maze_game.hpp"
 
-
+// Returns a pointer to a maze created based on a given maze factory.
 maze* maze_game::create_maze(maze_factory& factory){
     maze* mp = factory.make_maze();
     createdMazes.insert(mp);
@@ -17,28 +17,33 @@ maze* maze_game::create_maze(maze_factory& factory){
     return mp;
 }
 
+// Returns a pointer to a room created based on a given maze factory, and stores the pointer for later delete.
 room* maze_game::create_room(maze_factory& factory){
     room* rp = factory.make_room();
     createdRooms.insert(rp);
     return rp;
 }
 
+// Returns a pointer to a door created based on a given maze factory, and stores the pointer for later delete.
 door* maze_game::create_door(maze_factory& factory, room& room1, room& room2){
     door* dp = factory.make_door(room1, room2);
     createdDoors.insert(dp);
     return dp;
 }
 
+// Returns a pointer to a wall created based on a given maze factory, and stores the pointer for later delete.
 wall* maze_game::create_wall(maze_factory& factory){
     wall* wp = factory.make_wall();
     createdWalls.insert(wp);
     return wp;
 }
 
+// Destructor that calls deleteAll()
 maze_game::~maze_game() {
     deleteAll();
 }
 
+// Method that frees all the memory and deletes the pointers used for the mazes, rooms, walls and doors.
 void maze_game::deleteAll() {
     for(auto it = createdMazes.begin(); it != createdMazes.end(); ){
         delete *it;
@@ -58,6 +63,7 @@ void maze_game::deleteAll() {
     }
 }
 
+// Prints the Game to the output string.
 ostream& operator<<(ostream& os, const maze_game& mg){
     os << "Maze Game: ";
     int count = 1;
